420a5f867cbf0cb8fe845fdd513467bc7710d0be
bobcui <bobcui@yeah.net>
2014-12-03 13:55:32 +0800

    add param

--- ipGeoServer.js
+++ ipGeoServer.js
@@ -1,11 +1,12 @@
+var _ = require('underscore')
 var http = require('http')
 var url = require('url')
 var querystring = require('querystring')
 var geoip = require('geoip-lite')
-var _ = require('underscore')
+var argv = require('optimist').argv
 
-var host = '127.0.0.1'
-var port = 5127
+var host = argv.h || argv.host || '127.0.0.1'
+var port = argv.p || argv.port || 5127
 
 var defaultSearch = ['country', 'city']
 var defaultGeoData = {
@@ -21,14 +22,17 @@ req: ip=x.x.x.x&ip=y.y.y.y&query=country&query=city
 example: http://192.168.186.102:5117/?ip=180.154.237.252&ip=101.154.237.2&query=country&query=city&query=region&query=ll&query=range
 res: JSON.stringify({
         'x.x.x.x': {
-            'country':
-            'city':
+            'country': 'CN',
+            'city': 'beijing'
         },
         'y.y.y.y': {
-            'country':
-            'city':
+            'country': 'CN'
+            'city': 'shanghai'
         },
-        'z.z.z.z': null (if wrong)
+        'z.z.z.z': {    // if 192.168.x.x / 172.16.x.x / 10.x.x.x
+            'country': '',
+            'city': ''
+        }
     })
 */
 http.createServer(function (req, res) {
